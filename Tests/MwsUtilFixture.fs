e7c9179fcef74c66be1688281acdee61555ae9cb
Mark Miller <developmentalmadness@gmail.com>
2015-04-08 16:27:31 -0600

    Added Tests project

---
+++ Tests/MwsUtilFixture.fs
@@ -0,0 +1,11 @@
+﻿namespace dvMENTALmadness.AmazonMwsProxy.Tests
+
+module MwsUtilFixture =
+    open Xunit
+    open Swensen.Unquote
+
+    [<Fact>]
+    let ``Test Name`` () =
+        let expected = "a"
+        let actual = "a"
+        test <@ expected = actual @>
