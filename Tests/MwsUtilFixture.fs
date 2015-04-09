5f06aed9af8aa582511ed6966dec0ed7ee5f9dc2
Mark Miller <developmentalmadness@gmail.com>
2015-04-08 19:58:16 -0600

    passing encoding, signing tests

--- Tests/MwsUtilFixture.fs
+++ Tests/MwsUtilFixture.fs
@@ -1,6 +1,7 @@
 ﻿namespace dvMENTALmadness.AmazonMwsProxy.Tests
 
 module MwsUtilFixture =
+    open System
     open Xunit
     open Swensen.Unquote
     open dvMENTALmadness.amazon.client.MwsUtil
@@ -110,4 +111,44 @@ module MwsUtilFixture =
     //*****************************
     //     urlEncode
     //*****************************
+    
+    [<Theory>]
+    [<InlineData("http://www.amazon.com/?wierdStuff=~*'!", "http%3A%2F%2Fwww.amazon.com%2F%3FwierdStuff%3D~%2A%27%21")>]
+    let ``urlEncode parsing matches aws library`` (target:string) (expected:string) =
+        let actual = urlEncode target false
+        test <@ expected = actual @>
+    
+    [<Theory>]
+    [<InlineData("http://www.amazon.com/?wierdStuff=~*'!", "http%3A//www.amazon.com/%3FwierdStuff%3D~%2A%27%21")>]
+    let ``urlEncode path parsing matches aws library`` (target:string) (expected:string) =
+        let actual = urlEncode target true
+        test <@ expected = actual @>
 
+    //*****************************
+    //   signParameters
+    //*****************************
+
+    let ``signParameters creates API V2 string`` () =
+        let uri = Uri("https://mws.amazonservices.com/Products/2011-10-01")
+        let version = "2"
+        let signatureMethod = "HmacSHA256"
+        let secret = "foo_Bbar+baZToxeo9EYlwsmUa0EZTosQp1MJ7-XK"
+        let actual = signParameters uri 
+                                    version
+                                    signatureMethod 
+                                    [("AWSAccessKeyId","A1B2C3D4E5F6G7H8I9J0"); 
+                                    ("MarketplaceId","ATVPDKIKX0DER"); 
+                                    ("SellerId","A1B2C3D4E5F6G"); 
+                                    ("Timestamp","2015-04-09T00:07:57Z"); 
+                                    ("Action","GetMatchingProductForId"); 
+                                    ("IdList.Id.1","B000HDP3MA"); 
+                                    ("IdList.Id.2","B000X63K7G"); 
+                                    ("IdList.Id.3","B009VGDVYQ"); 
+                                    ("IdList.Id.4","B00LIP5MEK"); 
+                                    ("IdList.Id.5","B004EY27X6"); 
+                                    ("IdType","ASIN"); 
+                                    ("MWSAuthToken","A1B2C3D4E5F6G7H8I9J0"); 
+                                    ("Version","2011-10-01"); ]
+                                    secret
+
+        test <@ "cEwITdwUQQTpJ4xDx4Ud9Hx2iCF4d094LFcdZffT1Jw=" = actual.Value @>
