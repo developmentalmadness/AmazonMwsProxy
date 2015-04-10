64a01c8bfc17593f36f1c30f0f90ffe3a5cd5939
Mark Miller <developmentalmadness@gmail.com>
2015-04-09 18:05:39 -0600

    creating request pipeline

--- AmazonMwsProxy/MwsRequest.fs
+++ AmazonMwsProxy/MwsRequest.fs
@@ -1,64 +1,210 @@
 ﻿namespace dvMENTALmadness.amazon.client
 
-module Example =
+module Request =
+    open System
+    open System.Text
+    open System.Collections.Generic
+    open System.Net
+    open System.Globalization
+    open System.IO
+    open log4net
+    open dvMENTALmadness.Core.Logging
     
-    type A =
-        | First of string
-        | Second of string
-    
-    type B =
-        | First of string
-        | Second of string
-        | Third of string
+    let log = LogManager.GetLogger("Request")
 
-    let createA f s =
-        [A.First f; A.Second s;]
+    [<Literal>]
+    let APPLICATION_NAME = "AppName"
+    [<Literal>]
+    let APPLICATION_VERSION = "AppVersion"
+    [<Literal>]
+    let SERVICE_VERSION = "ServiceVersion"
+    [<Literal>]
+    let SIGNATURE_METHOD = "SignatureMethod"
+    [<Literal>]
+    let SIGNATURE_VERSION = "SignatureVersion"
+    [<Literal>]
+    let ENDPOINT = "Endpoint"
+    [<Literal>]
+    let SERVICE_PATH = "ServicePath"
+    [<Literal>]
+    let MAX_ERROR_RETRY = "MaxErrorRetry"
+    [<Literal>]
+    let PROGRAMMING_LANGUAGE = "F#"
+    [<Literal>]
+    let AWS_ACCESS_KEY = "AwsAccessKey"
+    [<Literal>]
+    let TIMESTAMP = "Timestamp"
+    [<Literal>]
+    let SIGNATURE = "Signature"
 
-    let createB f s t =
-        [B.First f; B.Second s; B.Third t]
+    type responseHeaderMetadata = {
+        RequestId:string;
+        ResponseContext:string list;
+        Timestamp:string;
+        QuotaMax:int option;
+        QuotaRemaining:int option;
+        QuotaResetsAt:DateTime option
+    }
 
-    let toParamA elem =
-        match elem with
-        | A.First f -> "First", f
-        | A.Second s -> "Second", s
+    let createConfig (appName:string) (appVersion:string) (serviceVersion:string) (signatureMethod:string) (signatureVersion:string) (endpoint:string) (servicePath:string) (awsAccessKey:string) =
+        [|(APPLICATION_NAME, appName); (APPLICATION_VERSION, appVersion); (SERVICE_VERSION, serviceVersion); 
+            (SIGNATURE_METHOD, signatureMethod); (SIGNATURE_VERSION, signatureVersion); (ENDPOINT, endpoint); 
+            (SERVICE_PATH, servicePath); (AWS_ACCESS_KEY, awsAccessKey)|] |> dict
 
-    let toParamB elem =
-        match elem with
-        | B.First f -> "First", f
-        | B.Second s -> "Second", s
-        | B.Third t -> "Third", t
+    let getServiceUrl (config:IDictionary<string, string>) =
+        Uri(Uri(config.[ENDPOINT]), config.[SERVICE_PATH])
 
-    let rec toParam f state args =
+    let getUserAgent (config:IDictionary<string, string>) =
+        let data = StringBuilder()
+        data.Append(config.[APPLICATION_NAME])
+            .Append("/")
+            .Append(config.[APPLICATION_VERSION])
+            .Append(" (")
+            .Append("Language=")
+            .Append(PROGRAMMING_LANGUAGE)
+            .Append(")")
+            .ToString()
+
+    let getHttpClient config =
+        let url = getServiceUrl config
+        let request = WebRequest.Create(url) :?> HttpWebRequest
+        //TODO: add support for proxy here (after I add it to the config)
+        request.UserAgent <- getUserAgent config
+        request.Method <- "POST"
+        request.Timeout <- 50000 // TODO: make timetout cnofigurable
+        request.ContentType <- "application/x-www-form-urlencoded; charset=utf-8"
+        //TODO: add support for headers (loop through configured headers and set request.Headers[key] <- value
+        request
+
+    let addRequiredParameters config request =
+        let serviceUri = getServiceUrl config
+        let request' = (TIMESTAMP,System.DateTimeOffset.UtcNow.ToString("o"))::(AWS_ACCESS_KEY, config.[AWS_ACCESS_KEY])::request
+        let signature = MwsUtil.signParameters serviceUri config.[SIGNATURE_VERSION] config.[SIGNATURE_METHOD] request' config.[AWS_ACCESS_KEY]
+        match signature with
+        | Some x -> (SIGNATURE, x)::request'
+        | None -> failwith "Failed to sign request parameters for request."
+
+    let getParametersAsString request =
+        let data = StringBuilder()
+        let build ((k:string), (v:string)) =
+            data.Append(k)
+                .Append('=')
+                .Append(MwsUtil.urlEncode v false)
+                .Append("&") |> ignore
+
+        request 
+            |> List.sortBy fst
+            |> List.iter (fun x -> build x)
+        
+        data.Remove(data.Length - 2, 1).ToString()
+
+    let getResponseHeaderMetadata (response:HttpWebResponse) =
+        let requestId = response.GetResponseHeader("x-mws-request-id")
+        let timestamp = response.GetResponseHeader("x-mws-timestamp")
+        let ctxStr = response.GetResponseHeader("x-mws-response-context")
+        let ctx = ctxStr.Split(',')
+
+        let qmax = 
+            match Double.TryParse(response.GetResponseHeader("x-mws-quota-max")) with
+            | (false, _) -> None
+            | (true, x) -> Some(Convert.ToInt32(x))
+        let qleft =
+            match Double.TryParse(response.GetResponseHeader("x-mws-quota-remaining")) with
+            | (false, _) -> None
+            | (true, x) -> Some(Convert.ToInt32(x))
+        let qreset =
+            match DateTime.TryParse(response.GetResponseHeader("x-mws-quota-resetsOn"), CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal) with
+            | (false, _) -> None
+            | (true, x) -> Some x
+
+        { 
+            RequestId = requestId;
+            ResponseContext = ctx |> Array.toList;
+            Timestamp = timestamp;
+            QuotaMax = qmax;
+            QuotaRemaining = qleft;
+            QuotaResetsAt = qreset; 
+        }
+
+    let executeRequest config request =
+        let queryString = request
+                            |> addRequiredParameters config
+                            |> getParametersAsString
+        
+        let buffer = Encoding.UTF8.GetBytes(queryString)
+
+        // retryBuilder ??
+
+        try
+            let http = getHttpClient config
+            use client = http.GetRequestStream()
+            client.Write(buffer, 0, buffer.Length)
+
+            use response = http.GetResponse() :?> HttpWebResponse
+            let message = response.StatusDescription
+            let meta = getResponseHeaderMetadata response
+            use rdr = new StreamReader(response.GetResponseStream(), Encoding.UTF8)
+
+            //TODO: parse response before returning
+            response.StatusCode, rdr.ReadToEnd()
+
+            //TODO: add retry loop when status code is InternalServerError (500)
+
+            // throw custom exception with statusCode and body
+        with 
+        | :? WebException as ex -> 
+        (
+            error log ex "An error occurred during executeRequest"
+            
+            use errResponse = ex.Response :?> HttpWebResponse
+            let errStatus, errBody = match errResponse with
+                                        | null -> reraise()
+                                        | er -> 
+                
+                                            use errRdr = new StreamReader(er.GetResponseStream(), Encoding.UTF8)
+                                            er.StatusCode, errRdr.ReadToEnd()
+
+            // retry ?
+
+            // throw custom exeception with errStatus and errBody
+            reraise()
+        )
+        | ex -> (
+                error log ex "An error occurred during executeRequest"
+                reraise()
+        )
+
+    // convertQueryStringToByteArray
+    // 
+
+
+    let rec toParamList fn args state =
         match args with
         | [] -> state
         | head::tail -> 
-            let state' = (f head)::state
-            toParam f state' tail
+            state 
+                |> (fn head)
+                |> List.fold (fun acc elem -> elem::acc) state
+                |> toParamList fn tail
 
-    let toA args =
-        toParam toParamA [] args
-
-    let toB args =
-        toParam toParamB [] args
-
-    let argA = createA "one" "two"
-    let argB = createB "one" "two" "three"
-
-    let resultA = argA |> toA
-    let resultB = argB |> toB
-
-
-
-
-    type IdType = ASIN | GCID | SellerSKU | UPC | EAN | ISBN | JAN
-
-    let rec toIdParams (prefix:string) target (idx:int) (ids:string list) =
+    let rec unmapList (prefix:string) target (idx:int) (ids:string list) =
         match ids with
         | [] -> target
         | head::tail -> 
             let p = (System.String.Concat(prefix, '.', idx), head)::target
             let nidx = idx + 1
-            toIdParams prefix p nidx tail
+            unmapList prefix p nidx tail
+
+
+module Products =
+    [<Literal>]
+    let SERVICE_VERSION = "2011-10-01"
+    [<Literal>]
+    let DEFAULT_SERVICE_PATH = "Products/2011-10-01"
+
+
+
+    type IdType = ASIN | GCID | SellerSKU | UPC | EAN | ISBN | JAN
 
     // GetCompetitivePricingForASINRequest
     type P =
@@ -66,16 +212,21 @@ module Example =
         | SellerId of string
         | ASINList of string list
         | MWSAuthToken of string
-    
-    let getCompetitivePricingForASINRequest marketplaceId sellerId asinList mwsAuthToken =
-        [P.MarketplaceId marketplaceId; P.SellerId sellerId; P.ASINList asinList; P.MWSAuthToken mwsAuthToken]
+        | Action of string
+        | Version of string
 
-    let toParamP elem state = 
+    let unmapP elem state = 
         match elem with
             | P.MarketplaceId m -> [("MarketplaceId", m)]
             | P.SellerId s -> [("SellerId", s)]
-            | P.ASINList ids -> ids |> toIdParams "ASINList.ASIN" state 1
+            | P.ASINList ids -> ids |> Request.unmapList "ASINList.ASIN" state 1
             | P.MWSAuthToken t -> [("MWSAuthToken", t)]
+            | P.Action a -> [("Action", a)]
+            | P.Version v -> [("Version", v)]
+    
+    let getCompetitivePricingForASINRequest marketplaceId sellerId asinList mwsAuthToken =
+        [P.MarketplaceId marketplaceId; P.SellerId sellerId; P.ASINList asinList; 
+            P.MWSAuthToken mwsAuthToken; P.Action "GetCompetitivePricingForASIN"; P.Version SERVICE_VERSION]
 
     // GetMatchingProductForIdRequest
     type M =
@@ -84,34 +235,32 @@ module Example =
         | IdType of IdType
         | IdList of string list
         | MWSAuthToken of string
+        | Action of string
+        | Version of string
 
-    let getMatchingProductForIdRequest marketplaceId sellerId idType idList mwsAuthToken =
-        [M.MarketplaceId marketplaceId; M.SellerId sellerId; M.IdType idType; M.IdList idList; M.MWSAuthToken mwsAuthToken]
-
-    let toParamM elem state = 
+    let unmapM elem state = 
         match elem with
             | M.MarketplaceId m -> [("MarketplaceId", m)]
             | M.SellerId s -> [("SellerId", s)]
             | M.IdType t -> [("IdType", sprintf "%A" t)]
-            | M.IdList ids -> ids |> toIdParams "ListId.Id" state 1
+            | M.IdList ids -> ids |> Request.unmapList "ListId.Id" state 1
             | M.MWSAuthToken t -> [("MWSAuthToken", t)]
-        
+            | M.Action a -> [("Action", a)]
+            | M.Version v -> [("Version", v)]
+
+    let getMatchingProductForIdRequest marketplaceId sellerId idType idList mwsAuthToken =
+        [M.MarketplaceId marketplaceId; M.SellerId sellerId; M.IdType idType; M.IdList idList; 
+            M.MWSAuthToken mwsAuthToken; M.Action "GetMatchingProductForId"; M.Version SERVICE_VERSION]
+(*    
     let argM = getMatchingProductForIdRequest "mpid" "sId" ASIN ["Key1"; "Key2"; "Key3"; "Key4"; "Key5"] "mwstoken"
     let argP = getCompetitivePricingForASINRequest "mpid" "sId" ["Key1"; "Key2"; "Key3"; "Key4"; "Key5"] "mwstoken"
 
-    let rec toParamList toParam state args =
-        match args with
-        | [] -> state
-        | head::tail -> 
-            let state' = (toParam head state)
-            let state'' = List.fold (fun acc elem -> elem::acc) state' state
-            toParamList toParam state'' tail
-    
     let toM args =
-        toParamList toParamM [] argM
+        Request.toParamList unmapM args [] 
     let toP args =
-        toParamList toParamP [] argP
+        Request.toParamList unmapP args []
         
-    let test = toParamList toParamM [] argM
-    let test' = toParamList toParamP [] argP
+    let test = toM argM
+    let test' = toP argP
 
+*)
--- AmazonMwsProxy/MwsUtil.fs
+++ AmazonMwsProxy/MwsUtil.fs
@@ -1,5 +1,48 @@
 ﻿namespace dvMENTALmadness.amazon.client
 
+module Retry =
+    open System.Threading
+    open System
+
+    type RetryParams = {
+        maxRetries : int; waitBetweenRetries : int
+        }
+
+    let defaultRetryParams = {maxRetries = 3; waitBetweenRetries = 1000}
+
+    type RetryMonad<'a> = RetryParams -> 'a
+    let rm<'a> (f : RetryParams -> 'a) : RetryMonad<'a> = f
+
+    let internal retryFunc<'a> (f : RetryMonad<'a>) =
+        rm (fun retryParams -> 
+            let rec execWithRetry f i e =
+                match i with
+                | n when n = retryParams.maxRetries -> raise e
+                | _ -> 
+                    try
+                        f retryParams
+                    with 
+                    | e -> Thread.Sleep(retryParams.waitBetweenRetries); execWithRetry f (i + 1) e
+            execWithRetry f 0 (Exception())
+            ) 
+
+    
+    type RetryBuilder() =
+        
+        member this.Bind (p : RetryMonad<'a>, f : 'a -> RetryMonad<'b>)  =
+            rm (fun retryParams -> 
+                let value = retryFunc p retryParams
+                f value retryParams                
+            )
+
+        member this.Return (x : 'a) = fun defaultRetryParams -> x
+
+        member this.Run(m : RetryMonad<'a>) = m
+
+        member this.Delay(f : unit -> RetryMonad<'a>) = f ()
+
+    let retry = RetryBuilder()
+
 // consider posting on: http://codereview.stackexchange.com/
 module MwsUtil =
     open System
